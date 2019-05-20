06046fa681c3063f8314e8698ad39ab1de826d10
AElmoznino <andrewlaufer@hotmail.com>
2019-05-20 22:18:23 +0200

    feat: mobsters are draggable

--- app/components/Mobsters/Mobsters.js
+++ app/components/Mobsters/Mobsters.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import styles from './Mobsters.css'
 import avatarImage from '../../assets/avatar.png'
 import githubButton from '../../assets/github.png'
@@ -14,7 +15,7 @@ const Mobsters = ({
   clickGuestButton,
   clickRemoveUser,
   isEditing,
-  setIsEditing,
+  onDragEnd,
   setUsername,
   username
 }) => (
@@ -23,6 +24,9 @@ const Mobsters = ({
       <div>
         <div className={styles.headerWrap}>
           <h4>Mobsters</h4>
+          <span className={styles.editLabel}>
+            {isEditing ? 'Spara' : 'Redigera'}
+          </span>
           <img
             className={styles.editButton}
             onClick={clickEditButton}
@@ -31,31 +35,60 @@ const Mobsters = ({
         </div>
       </div>
       <ul>
-        {!!activeUsers.length &&
-          activeUsers.map((user, index) => (
-            <li className={styles.mobster} key={index}>
-              <div className={styles.avatarWrap}>
-                <img
-                  className={styles.avatar}
-                  src={user.avatar ? user.avatar : avatarImage}
-                />
-                {index === 0 && <span className={styles.driverDot} />}
-                {index === 1 && <span className={styles.navigatorDot} />}
-              </div>
-              <div>
-                <p className={styles.name}>{user.name}</p>
-                <p className={styles.githubName}>{user.githubName}</p>
-              </div>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {!!activeUsers.length &&
+                  activeUsers.map((user, index) => (
+                    <Draggable
+                      key={index}
+                      draggableId={user.name}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <li
+                          className={styles.mobster}
+                          key={index}
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <div className={styles.avatarWrap}>
+                            <img
+                              className={styles.avatar}
+                              src={user.avatar ? user.avatar : avatarImage}
+                            />
+                            {index === 0 && (
+                              <span className={styles.driverDot} />
+                            )}
+                            {index === 1 && (
+                              <span className={styles.navigatorDot} />
+                            )}
+                          </div>
+                          <div>
+                            <p className={styles.name}>{user.name}</p>
+                            <p className={styles.githubName}>
+                              {user.githubName}
+                            </p>
+                          </div>
 
-              {isEditing && (
-                <img
-                  className={styles.editButton}
-                  onClick={() => clickRemoveUser(user.name)}
-                  src={removeButton}
-                />
-              )}
-            </li>
-          ))}
+                          {isEditing && (
+                            <img
+                              className={styles.editButton}
+                              onClick={() => clickRemoveUser(user.name)}
+                              src={removeButton}
+                            />
+                          )}
+                        </li>
+                      )}
+                    </Draggable>
+                  ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </ul>
     </div>
 
