e4ab17541f9d687421959ac875329310333940e3
André Elmoznino Laufer <andre.elmoznino@valtech.com>
2019-10-18 12:29:24 +0200

    feat: add animation on break screen, remove circular progress indicator

--- app/components/Break/Break.js
+++ app/components/Break/Break.js
@@ -1,12 +1,13 @@
 // @flow
 
 import React, { useContext } from 'react'
+import Lottie from 'react-lottie'
 import { FormattedMessage } from 'react-intl'
-import { CircularProgressbarWithChildren } from 'react-circular-progressbar'
 import { SettingsStoreContext } from '../../store/store'
 import { formatTime } from '../../utils/formatTime'
 import styles from './Break.css'
 import ResetButton from '../Button/ResetButton'
+import * as animationData from '../../assets/animations/break.json'
 
 type BreakProps = {
   resetBreak: () => void
@@ -17,14 +18,18 @@ const Break = ({ resetBreak }: BreakProps) => {
 
   const progress = (breakTimeLeft / breakDuration) * 100
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData.default,
+    rendererSettings: { preserveAspectRatio: 'xMidYMid slice' }
+  }
+
   return (
     <div className={styles.breakWrap}>
       <div>
-        <CircularProgressbarWithChildren value={progress}>
-          <p className={styles.text}>
-            <FormattedMessage id="break" />: {formatTime(breakTimeLeft)}
-          </p>
-        </CircularProgressbarWithChildren>
+        <Lottie options={defaultOptions} height={500} width={500} />
+        <p className={styles.text}>{formatTime(breakTimeLeft)}</p>
         <ResetButton handleClick={resetBreak} id="resetBreak" />
       </div>
     </div>
